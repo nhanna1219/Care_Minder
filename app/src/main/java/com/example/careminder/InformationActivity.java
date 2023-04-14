ca106d5e1c00ed4a7cf52e40a288591678f446af
Nhân Nguyễn Trọng <19521940@ms.uit.edu.vn>
2023-04-14 20:37:57 +0700

    first commit

---
+++ app/src/main/java/com/example/careminder/InformationActivity.java
@@ -0,0 +1,14 @@
+package com.example.careminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
---
+++ app/src/main/java/com/example/careminder/WaterActivity.java
@@ -0,0 +1,14 @@
+package com.example.careminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water);
+    }
+}
