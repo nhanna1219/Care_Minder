0a003aa2f32cb0e90fe8569ab9a914bbe650f1a7
Nhân Nguyễn Trọng <19521940@ms.uit.edu.vn>
2023-04-19 12:02:03 +0700

    Add resources to project

--- app/src/main/java/com/example/careminder/MainActivity.java
+++ app/src/main/java/com/example/careminder/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.careminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+//    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
