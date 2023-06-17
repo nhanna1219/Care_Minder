cbce5270d75567c9373f2719546c1219decb089c
nqkhanh2002 <20521452@gm.uit.edu.vn>
2023-06-17 16:20:35 +0700

    up

--- app/src/main/java/com/example/careminder/Activity/Setting/EditProfileActivity.java
+++ app/src/main/java/com/example/careminder/Activity/Setting/EditProfileActivity.java
@@ -3,9 +3,12 @@ package com.example.careminder.Activity.Setting;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import com.example.careminder.Activity.Home.HomeActivity;
 import com.example.careminder.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -15,7 +18,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class EditProfileActivity extends AppCompatActivity {
-    TextView fullName, email;
+    TextView fullName, email, back;
     FirebaseAuth auth;
     FirebaseFirestore firestore;
     String userID;
@@ -24,6 +27,7 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         fullName = findViewById(R.id.rectangleTextView);
+        back = findViewById(R.id.settingsTextView);
         auth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
         userID = auth.getCurrentUser().getUid();
@@ -36,6 +40,13 @@ public class EditProfileActivity extends AppCompatActivity {
                 fullName.setText(value.getString("name"));
 //                email.setText(value.getString("email"));
 
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditProfileActivity.this, SettingActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/careminder/Activity/Setting/HelpSettingActivity.java
+++ app/src/main/java/com/example/careminder/Activity/Setting/HelpSettingActivity.java
@@ -2,15 +2,27 @@ package com.example.careminder.Activity.Setting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.careminder.Activity.Home.HomeActivity;
 import com.example.careminder.R;
 
 public class HelpSettingActivity extends AppCompatActivity {
-
+    TextView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_setting);
+        back = findViewById(R.id.settingsTextView);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelpSettingActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
